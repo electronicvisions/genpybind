ae51ae0ed7564504fa50b2ae6722531f6629ac54
Eric Müller <mueller@kip.uni-heidelberg.de>
2019-08-27 11:42:19 +0200

    Fix default argument not using fully-qualfied class name
    Change-Id: I50314ff8ffdb36223cab67fbb8cbfb2c90485fa9

--- tests/default_arguments.h
+++ tests/default_arguments.h
@@ -10,14 +10,38 @@ void GENPYBIND(visible) function_builtin(int x = 5, bool arg = true) {}
 void GENPYBIND(visible) function_class(X x = X()) {}
 
 namespace example {
-struct GENPYBIND(visible) Y {
-  static constexpr int N = 42;
+
+struct GENPYBIND(visible) Z {
+	static constexpr int max = 17;
 };
+
+struct GENPYBIND(visible) Y {
+
+  static constexpr int N = 42;
+
+  Y(int v = 0) : _m(v) {};
+
+private:
+  int _m;
+};
+
 void GENPYBIND(visible) function_class_in_namespace(Y y = Y()) {}
+
+
+void GENPYBIND(visible) function_class_in_namespace_with_parameter(Y y = Y(Z::max)) {}
+
+
 } // namespace example
 
+
 void GENPYBIND(visible)
-    function_class_outside_namespace(example::Y y = example::Y()) {}
+function_class_outside_namespace(example::Y y = example::Y()) {}
+
+void GENPYBIND(visible)
+function_class_outside_namespace_with_parameter_namespaced(::example::Y y = ::example::Y(::example::Z::max)) {}
+
+void GENPYBIND(visible)
+function_class_outside_namespace_namespaced(::example::Y y = ::example::Y()) {}
 
 // TODO: genpybind uses `example::Y::N` as default argument?
 /*
@@ -34,6 +58,7 @@ void GENPYBIND(visible)
 */
 
 // TODO: braced initialization not supported in default argument
+// Not sure if pybind11 supports this at all...
 /*
 void GENPYBIND(visible) function_braced(X x = {}) {}
 namespace example {
