7464a87a90c275adc2418c744fdfc068195183d0
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2020-04-30 11:39:32 +0200

    Split generated code into independent functions
    Change-Id: Ie84da9163b04181452fc41b8053de59283210d03

--- genpybind/expose.py
+++ genpybind/expose.py
@@ -50,6 +50,8 @@ def expose_as(
         return py::reinterpret_borrow<py::object>((PyObject*)tinfo->type);
     }}
 
+    {functions}
+
     PYBIND11_MODULE({module}, {var}) {{
     {var}.doc() = {doc};
     {statements}
@@ -59,9 +61,12 @@ def expose_as(
     var = "m"
     registry = Registry(tags=tags)
 
+    toplevel_index = 0
     statements = []  # type: List[Text]
     pending_declarations = []  # type: List[Tuple[Declaration, Text]]
     postamble_declarations = []  # type: List[Tuple[Declaration, Text]]
+    functions = []
+    functions_prefix= []
 
     def handle_return(declaration, value, postamble_only=False):
         # type: (Declaration, Union[None, Text, Tuple[Declaration, Text]], bool) -> None
@@ -89,6 +94,12 @@ def expose_as(
     for declaration in toplevel_declarations:
         for value in utils.flatten(declaration.expose(var, registry)):
             handle_return(declaration, value)
+        function = "\n".join(s for s in statements)
+        functions_prefix.append(
+            "void function_{}(py::module& m)".format(toplevel_index) + "\n{\nstatic_cast<void>(m);\n")
+        functions.append(function)
+        toplevel_index += 1
+        statements = []
 
     while pending_declarations:
         declaration, parent = pending_declarations.pop(0)
@@ -100,6 +111,47 @@ def expose_as(
         for value in utils.flatten(declaration.expose_later(var, parent, registry)):
             handle_return(declaration, value, postamble_only=True)
 
+    def find_recursive_attribute(name, lines, parent_names = []):
+        for line in lines:
+            if line.startswith("auto {}".format(name)):
+                args = line.split('=')[1].split('(')[1].split(')')[0].split(', ')
+                if args[0] == 'm':
+                    parent_names = [args[1]] + parent_names
+                    return parent_names
+                else:
+                    parent_names = [args[1]] + parent_names
+                    return find_recursive_attribute(args[0], lines, parent_names)
+        raise Exception("Did not find definition of {}".format(name))
+
+    for i in range(len(statements)):
+        lines = statements[i].split('\n')
+        l = 0
+        for line in lines:
+            global_type = line.split('.')[0]
+            if global_type.startswith("genpybind") and not line.split('.')[1].startswith("attr"):
+                # non-attr can be reordered, do it
+                for f in range(len(functions)):
+                    if functions[f].find(global_type) != -1:
+                        print("/* non-attr: {} */".format(line))
+                        functions[f] = functions[f] + "\n" + line + "\n"
+                        lines.remove(line)
+            if global_type.startswith("genpybind") and line.split('.')[1].startswith("attr"):
+                # attr can't be reordered, make it use the module to get the object
+                parent_names = find_recursive_attribute(global_type, ("\n".join(functions)).split("\n"))
+                assert parent_names is not None
+                print("/* attr: {} */".format(parent_names))
+                split = line.split('.')
+                # FIXME: nesting of types not working
+                split[0] = "m" + "".join([".attr({})".format(attr) for attr in parent_names])
+                lines[l] = ".".join(split)
+            l += 1
+        statements[i] = "\n".join(lines)
+
+    for i in range(toplevel_index):
+        functions[i] = functions_prefix[i] + functions[i] + "\n}\n"
+
+    statements = ["function_{}(m);".format(i) for i in range(toplevel_index)] + statements
+
     return tpl.format(
         module=module,
         name=quote(module),
@@ -107,5 +159,6 @@ def expose_as(
         isystem="\n".join('#include <{}>'.format(f) for f in isystem or []),
         includes="\n".join('#include {}'.format(quote(f)) for f in includes or []),
         var=var,
-        statements="\n".join(statement for statement in statements),
+        functions="\n".join(function for function in functions),
+        statements="\n".join(statements)
     )
