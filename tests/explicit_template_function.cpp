086e532c5d1f34fdeede73071da3c4bc2f7f5266
Johann Klähn <johann@jklaehn.de>
2020-07-13 17:56:58 +0200

    Partially fix default arguments for function templates
    Change-Id: I246d279175bc7e12d9e74723e01959ccc26dec01

--- tests/explicit_template_function.cpp
+++ tests/explicit_template_function.cpp
@@ -6,7 +6,12 @@ template <typename T> T Klass::increase(const T &val) { return val + 1; }
 
 template <typename T> T frobnicate(const T &val) { return 42 - val; }
 
+template <typename T> T echo_with_default_argument(T val) {return val;}
+template <typename T> T echo_with_default_argument_2(T val) {return val;}
+
 template Something::Something(int);
 template int Klass::increase(const int &);
 template int frobnicate<int>(const int &);
 template double frobnicate<double>(const double &);
+template int echo_with_default_argument<int>(int);
+template int echo_with_default_argument_2<int>(int);
